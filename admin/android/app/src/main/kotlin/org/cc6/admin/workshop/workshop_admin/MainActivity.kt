efc5dde8d608949108702d2617845fc7cf9e03ab
ochamo <60203851+ochamo@users.noreply.github.com>
2022-09-17 00:25:39 -0600

    initial commit

---
+++ admin/android/app/src/main/kotlin/org/cc6/admin/workshop/workshop_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.cc6.admin.workshop.workshop_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile/android/app/src/main/kotlin/org/cc6/workshop/workshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.cc6.workshop.workshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
