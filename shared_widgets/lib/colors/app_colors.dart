cae3b567ba8dc2ae47b026e92f5e6cd78d43eb11
ochamo <60203851+ochamo@users.noreply.github.com>
2022-09-28 01:55:53 -0600

    feat: setup shared app colors and app theme

---
+++ shared_widgets/lib/colors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFF2196F3);
+  static const Color secondaryColor = Color(0xFFFF9800);
+  static const Color primaryDarkColor = Color(0xFF1976D2);
+  static const Color primaryLightColor = Color(0xFFBBDEFB);
+  static const Color textIconColor = Color(0xFFFFFFFF);
+  static const Color primaryTextColor = Color(0xFF212121);
+  static const Color secondaryTextColor = Color(0xFF757575);
+  static const Color dividerColor = Color(0xFFBDBDBD);
+  static const Color errorColor = Color(0xFFD32F2F);
+}
